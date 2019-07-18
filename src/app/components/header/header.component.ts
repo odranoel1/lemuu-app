1e84a9642001c8024302ee21e29fe89811a8fc23
Leo Gonzalez <leo.gonzalez@lava.mx>
2019-07-18 15:19:03 -0500

    Styles: Components and Views

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,9 +8,35 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   @Input() title: string;
+  @Input() image: string;
+  @Input() text: string;
+  @Input() notification:string;
 
-  constructor() { }
+  public show_title:boolean = false;
+  public show_picture:boolean = false;
+  public show_notification:boolean = false;
 
-  ngOnInit() {}
+  constructor() {
+  }
+
+  ngOnInit() {
+    if (this.text == 'true') {
+      this.show_title = true;
+    }
+
+    if (this.image == 'true') {
+      this.show_picture = true;
+    }
+
+    if (this.notification == 'true') {
+      this.show_notification = true;
+    }
+  }
 
 }
+
+/*
+create a flag
+change status of flag with html
+
+*/
